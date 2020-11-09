923c0dea5e1c4ae728297624dd3a7fe5cd444069
sami pentti <sami.pentti@gmail.com>
2020-11-09 19:52:06 +0200

    Tehty tehtävät osaan 11 asti

--- viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Kauppa.java
+++ viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Kauppa.java
@@ -3,16 +3,16 @@ package ohtu.verkkokauppa;
 public class Kauppa {
 
     private Varasto varasto;
-    private Pankki pankki;
     private Ostoskori ostoskori;
     private Viitegeneraattori viitegeneraattori;
     private String kaupanTili;
+    private Pankki pankki;
 
-    public Kauppa() {
-        varasto = Varasto.getInstance();
-        pankki = Pankki.getInstance();
-        viitegeneraattori = Viitegeneraattori.getInstance();
+    public Kauppa(Varasto varasto, Pankki pankki, Viitegeneraattori viitegeneraattori) {
         kaupanTili = "33333-44455";
+        this.pankki = pankki;
+        this.varasto = varasto;
+        this.viitegeneraattori = viitegeneraattori;
     }
 
     public void aloitaAsiointi() {
@@ -20,13 +20,13 @@ public class Kauppa {
     }
 
     public void poistaKorista(int id) {
-        Tuote t = varasto.haeTuote(id); 
+        Tuote t = varasto.haeTuote(id);
         varasto.palautaVarastoon(t);
     }
 
     public void lisaaKoriin(int id) {
         if (varasto.saldo(id)>0) {
-            Tuote t = varasto.haeTuote(id);             
+            Tuote t = varasto.haeTuote(id);
             ostoskori.lisaa(t);
             varasto.otaVarastosta(t);
         }
@@ -35,7 +35,7 @@ public class Kauppa {
     public boolean tilimaksu(String nimi, String tiliNumero) {
         int viite = viitegeneraattori.uusi();
         int summa = ostoskori.hinta();
-        
+
         return pankki.tilisiirto(nimi, viite, tiliNumero, kaupanTili, summa);
     }
 
---
+++ viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/VarastoInterface.java
@@ -0,0 +1,21 @@
+package ohtu.verkkokauppa;
+import java.util.HashMap;
+
+public interface VarastoInterface {
+
+    Tuote haeTuote(int id);
+
+    int saldo(int id);
+
+    void otaVarastosta(Tuote t);
+
+    void palautaVarastoon(Tuote t);
+
+    default void alustaTuotteet(HashMap<Tuote, Integer> saldot) {
+        saldot.put(new Tuote(1, "Koff Portteri", 3), 100);
+        saldot.put(new Tuote(2, "Fink Bräu I", 1), 25);
+        saldot.put(new Tuote(3, "Sierra Nevada Pale Ale", 5), 30);
+        saldot.put(new Tuote(4, "Mikkeller not just another Wit", 7), 40);
+        saldot.put(new Tuote(5, "Weihenstephaner Hefeweisse", 4), 15);
+    }
+}
